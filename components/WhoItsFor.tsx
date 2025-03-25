4f22cc1ffb3cb41959d55ba3852f7617d572d879
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-03-25 18:18:53 +0100

    feat: built the currency hub and the who it's for sections

---
+++ components/WhoItsFor.tsx
@@ -0,0 +1,18 @@
+export default function WhoItsFor() {
+  return (
+    <section className="flex flex-col justify-center items-center py-3 md:py-10 px-3 md:px-16 ">
+      <div className="bg-[#98E5FE] flex flex-col justify-center items-center space-y-7 p-5 rounded-lg shadow-lg w-full md:max-w-5xl">
+        <h3 className="bg-[#0066CC] text-white px-7 py-2 rounded-full -rotate-[15deg]">
+          Who It's For
+        </h3>
+        <p>
+          Are you a newcomer to Web3, a developer exploring blockchain
+          integration, or a crypto enthusiast seeking deeper insights? ByteChain
+          Academy is tailored for busy professionals, financial analysts needing
+          historical currency data, and anyone who prefers learning in
+          bite-sized, digestible chunks rather than lengthy courses.
+        </p>
+      </div>
+    </section>
+  );
+}
