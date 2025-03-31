9d4823fc8118f54a662e4d369cad9ac0b095363b
anushkasomani <somanianu956@gmail.com>
2025-03-31 12:27:04 +0530

    signin skeleton added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,24 @@
 "use client";
 import Login from "@/components/ui/login";
-
+import { useEffect, useState } from "react";
+import LoginSkeleton from "@/components/ui/LoginSkeleton";
+import { LOADIPHLPAPI } from "dns";
 export default function SignIn() {
-  return <Login />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LoginSkeleton />;
+  }
+
+  return (
+    <Login/>
+  );
+  
 }
--- components/ui/login.tsx
+++ components/ui/login.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
